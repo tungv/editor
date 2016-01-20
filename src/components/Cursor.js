a13c2776d2170af3cd50f02cb950682c2fb3d5e6
Tung Vu <me@tungv.com>
2016-01-20 09:05:16 +0700

    add blinking cursor

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -1,9 +1,64 @@
 import React from 'react';
 
-export default function Cursor(props) {
-  return (
-    <div style={{position: 'absolute', width: '2px', background: 'red', display: 'inline-block', marginLeft: '1px'}}
-      children={['\uFEFF']}
-    />
-  );
+const styles = {
+  root: {
+    position: 'absolute',
+    display: 'inline-block',
+  },
+  verticalLine: {
+    position: 'absolute',
+    width: '0',
+    borderLeft: '2px solid red',
+    marginLeft: '1px'
+  },
+  marker: {
+    width: '4px',
+    height: '4px',
+    background: 'red'
+  }
 }
+
+class Cursor extends React.Component {
+  state = {
+    visible: true,
+  };
+
+  constructor(props) {
+    super(props);
+    this._startBlinking();
+  }
+
+  componentWillUnmount() {
+    this._stopBlinking();
+  }
+
+  _startBlinking() {
+    this.blinkInterval = setInterval(::this._toggle, 750);
+  }
+
+  _stopBlinking() {
+    clearInterval(this.blinkInterval);
+  }
+
+  _toggle() {
+    this.setState(state => {
+      const { visible, ...rest } = state;
+      return {
+        visible: !visible,
+        ...rest,
+      }
+    })
+  }
+
+  render() {
+    const { visible } = this.state;
+    return (
+      <div key='cursor' style={{...styles.root, opacity: visible ? 1: 0}}>
+        <div key='vertical_line' style={styles.verticalLine} children={['\uFEFF']}/>
+        <div key='marker' style={styles.marker} children={['\uFEFF']}/>
+      </div>
+    );
+  }
+}
+
+export default Cursor;
