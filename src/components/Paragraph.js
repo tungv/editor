8cda148c7b64faecd42b19607f7aa5a6910302ea
Tung Vu <me@tungv.com>
2016-01-20 08:43:53 +0700

    fix spacing

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Cursor from './Cursor';
 
+function keepSpace(string) {
+  return string.split(' ').join('\u2005');
+}
+
 export default function Paragraph(props) {
   const { cursor } = props;
+  const content = keepSpace(props.content);
   return (<div style={{position: 'relative'}}>
-    <div style={{position: 'relative', wordWrap: 'break-word', width: '100%'}}>{ props.content }</div>
+    <div style={{position: 'relative', wordWrap: 'break-word', width: '100%'}}>
+      { content }
+    </div>
     { cursor &&
       <div style={{position: 'absolute', top: '0', wordWrap: 'break-word', width: '100%'}}>
         <span style={{visibility: 'hidden'}}>
-          { props.content.slice(0, cursor.characterIndex + 1) }
+          { content.slice(0, cursor.characterIndex + 1) }
         </span>
         <Cursor />
       </div>
